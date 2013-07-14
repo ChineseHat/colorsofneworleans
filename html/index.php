5d8da70f1c2ddbdf7b76a45202e61f3f12905275
Matt Turland <mturland@synacor.com>
2013-07-14 16:09:29 -0400

    Got configuration, templating, and database connection working

--- html/index.php
+++ html/index.php
@@ -1,15 +1,34 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php'; 
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Silex\Application(); 
+$app = new Silex\Application(array(
+	'debug' => true
+));
 
-$app['mustache'] = $app->share(function() {
-	return new \Mustache_Engine;
+$config = require __DIR__ . '/../config.php';
+foreach ($config as $key => $value)
+{
+	$app[$key] = $value;
+}
+
+$app['db'] = $app->share(function() use ($app) {
+	return new \PDO(
+		'mysql:host=' . $app['db_host'] . ';dbname=' . $app['db_name'],
+		$app['db_user'],
+		$app['db_pass']
+	);
 });
 
-$app->get('/', function() use ($app) { 
-	return $app['mustache']->render('Hello {{planet}}', array('planet' => 'World!'));
-}); 
+$app['mustache'] = $app->share(function() {
+	return new \Mustache_Engine(array(
+		'loader' => new \Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates', array('extension' => 'tpl')),
+	));
+});
 
-$app->run(); 
+$app->get('/', function() use ($app) {
+	$template = $app['mustache']->loadTemplate('test');
+	return $template->render(array('planet' => 'World'));
+});
+
+$app->run();
