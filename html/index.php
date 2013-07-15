a1f884ed165ab51acbce09e638e8102255e73544
Brian Berlin <bri@nberl.in>
2013-07-15 12:47:49 -0500

    added google analytics to index.php

--- html/index.php
+++ html/index.php
@@ -156,5 +156,15 @@ $twitter =  new ZendService\Twitter\Twitter(array(
     <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="/javascript/scripts.js"></script>
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', '<?php print $app['google_analytics_id']; ?>', 'colorsofneworleans.com');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>
