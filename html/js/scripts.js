5992cb7dc138e74680948a6f4821907128e18906
Brian Berlin <bri@nberl.in>
2013-07-21 14:00:47 -0500

    tweaks to style

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 
+  var uri = window.location.pathname.replace("/","");
+  if (uri == '') { uri = 'home'; }
+  $("#menu a").each(function(){
+    if($(this).attr("class").indexOf(uri) != -1){
+      $(this).addClass('active');
+    }
+  });
 });
